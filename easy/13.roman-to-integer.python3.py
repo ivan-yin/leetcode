a61556494ea35b4d4b65fc69ab78b66a3ad9e52f
nelson <020lilin@gmail.com>
2018-09-27 14:59:36 +0800

    roman to integer

--- easy/13.roman-to-integer.python3.py
+++ easy/13.roman-to-integer.python3.py
@@ -11,8 +11,8 @@
 #
 # Roman numerals are represented by seven different symbols: I, V, X, L, C, D
 # and M.
-# 
-# 
+#
+#
 # Symbol       Value
 # I             1
 # V             5
@@ -21,64 +21,89 @@
 # C             100
 # D             500
 # M             1000
-# 
+#
 # For example, two is written as II in Roman numeral, just two one's added
 # together. Twelve is written as, XII, which is simply X + II. The number
 # twenty seven is written as XXVII, which is XX + V + II.
-# 
+#
 # Roman numerals are usually written largest to smallest from left to right.
 # However, the numeral for four is not IIII. Instead, the number four is
 # written as IV. Because the one is before the five we subtract it making four.
 # The same principle applies to the number nine, which is written as IX. There
 # are six instances where subtraction is used:
-# 
-# 
+#
+#
 # I can be placed before V (5) and X (10) to make 4 and 9. 
 # X can be placed before L (50) and C (100) to make 40 and 90. 
 # C can be placed before D (500) and M (1000) to make 400 and 900.
-# 
-# 
+#
+#
 # Given a roman numeral, convert it to an integer. Input is guaranteed to be
 # within the range from 1 to 3999.
-# 
+#
 # Example 1:
-# 
-# 
+#
+#
 # Input: "III"
 # Output: 3
-# 
+#
 # Example 2:
-# 
-# 
+#
+#
 # Input: "IV"
 # Output: 4
-# 
+#
 # Example 3:
-# 
-# 
+#
+#
 # Input: "IX"
 # Output: 9
-# 
+#
 # Example 4:
-# 
-# 
+#
+#
 # Input: "LVIII"
 # Output: 58
 # Explanation: L = 50, V= 5, III = 3.
-# 
-# 
+#
+#
 # Example 5:
-# 
-# 
+#
+#
 # Input: "MCMXCIV"
 # Output: 1994
 # Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
-# 
 #
+#
+
+
 class Solution:
+    # I             1
+    # V             5
+    # X             10
+    # L             50
+    # C             100
+    # D             500
+    # M             1000
+    # 从后面开始变量，如果前面的值小于后面的，则用减法，否则相加
     def romanToInt(self, s):
         """
         :type s: str
         :rtype: int
         """
-        
+        kvs = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        data = 0
+        pivot = 0
+        lens = len(s)-1
+        while lens >= 0:
+            value = kvs[s[lens]]
+            if pivot <= value:
+                pivot = value
+                data += value
+            else:
+                data -= value
+            lens -= 1
+        return data
+
+
+Solution().romanToInt('MCMXCIV')
